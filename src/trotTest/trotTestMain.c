9b3d2131f2697e2ae588c114cd0d8f898122d03a
Jeremiah Martell <inlovewithGod@gmail.com>
2012-02-04 16:00:13 -0500

    Adding 2012 to copyrights.

--- src/trotTest/trotTestMain.c
+++ src/trotTest/trotTestMain.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2010,2011, Jeremiah Martell
+Copyright (c) 2010,2011,2012, Jeremiah Martell
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
